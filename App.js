22f89314bcc8437b71b8004e7f529c017e163f81
HaydenJenkins <danceingfrog96@gmail.com>
2024-08-25 19:50:55 -0400

    added title

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>RxReminder App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: 'orange',
+    fontSize: '30px',
+    fontWeight: 'bold',
   },
 });
